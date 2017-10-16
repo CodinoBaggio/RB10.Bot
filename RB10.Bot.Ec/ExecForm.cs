0a9e073f468e2f8086a7021d203bc453b556e526
Codino Baggio <codino.baggio10@gmail.com>
2017-10-16 15:45:59 +0900

    1つの画面で各ECサイト自動実行

---
+++ RB10.Bot.Ec/ExecForm.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RB10.Bot.Ec
+{
+    public partial class ExecForm : Form
+    {
+        private Library.Configuration _config;
+        private Library.ECSite.ECBase _ec;
+
+        public ExecForm()
+        {
+            InitializeComponent();
+
+            _config = new Library.Configuration(Properties.Settings.Default.BotConfigurationFileName);
+        }
+
+        private void ExecForm_Load(object sender, EventArgs e)
+        {
+            UrlTextBox.Text = _config.EcSite.Amazon.ItemUrl;
+            ShopNameTextBox.Text = _config.EcSite.Amazon.ShopName;
+            UpperLimitPriceTextBox.Text = _config.EcSite.Amazon.UpperLimitPrice.ToString();
+            LoginIDTextBox.Text = _config.EcSite.Amazon.LoginID;
+        }
+
+        private void RunAmazonButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (MailCheckBox.Checked)
+                {
+                    if (ToMailAddressTextBox.Text == "") throw new ApplicationException("送信先メールアドレスを入力してください。");
+                    if (MailPasswordTextBox.Text == "") throw new ApplicationException("メールパスワードを入力して下しさい。");
+                }
+                if (UrlTextBox.Text == "") throw new ApplicationException("商品URLを入力してください。");
+                if (ShopNameTextBox.Text == "") throw new ApplicationException("ショップ名を入力してください。");
+                if (UpperLimitPriceTextBox.Text == "") throw new ApplicationException("上限価格を入力してください。");
+                if (LoginIDTextBox.Text == "") throw new ApplicationException("ログインIDを入力してください。");
+                if (PasswordTextBox.Text == "") throw new ApplicationException("パスワードを入力してください。");
+
+                var param = new Library.ECSite.Amazon.AmazonParameters
+                {
+                    WebDriver = GetWebDriverType(),
+                    ItemUrl = UrlTextBox.Text,
+                    LoginID = LoginIDTextBox.Text,
+                    Password = PasswordTextBox.Text,
+                    PurchaseShopName = ShopNameTextBox.Text,
+                    UpperLimitPrice = Convert.ToDecimal(UpperLimitPriceTextBox.Text),
+                    FixedOrder = PurchaseCheckBox.Checked,
+                    IsSendMail = MailCheckBox.Checked,
+                    ToMailAddress = ToMailAddressTextBox.Text,
+                    MailPassword = MailPasswordTextBox.Text
+                };
+
+                _ec = new Library.ECSite.Amazon(param);
+                _ec.ExecutingStateChanged += Ec_ExecutingStateChanged;
+                _ec.Start();
+            }
+            catch (ApplicationException ex)
+            {
+                MessageBox.Show(ex.Message, Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void StopAmazonButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                _ec.Stop();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void MailCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            var chkBox = (CheckBox)sender;
+            MailPanel.Enabled = chkBox.Checked;
+        }
+
+        private Library.ECSite.ECBase.WebDriverType GetWebDriverType()
+        {
+            if (ChromeRadioButton.Checked) return Library.ECSite.ECBase.WebDriverType.Chrome;
+            if (IERadioButton.Checked) return Library.ECSite.ECBase.WebDriverType.IE;
+            if (FirefoxRadioButton.Checked) return Library.ECSite.ECBase.WebDriverType.Firefox;
+            if (SafariRadioButton.Checked) return Library.ECSite.ECBase.WebDriverType.Safari;
+
+            return Library.ECSite.ECBase.WebDriverType.Chrome;
+        }
+
+        private void Ec_ExecutingStateChanged(object sender, Library.ECSite.ECBase.ExecutingStateEventArgs e)
+        {
+            Invoke(new LogDelegate(AddLog), e.EcSite, DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss"), e.ReportState.ToString(), e.Message);
+        }
+
+        private class Log
+        {
+            public string EcSite { get; set; }
+            public string LogDate { get; set; }
+            public string Status { get; set; }
+            public string Message { get; set; }
+        }
+
+        private BindingList<Log> _logs { get; set; }
+        delegate void LogDelegate(string ecSite, string logDate, string status, string message);
+
+        private void AddLog(string ecSite, string logDate, string status, string message)
+        {
+            if (_logs == null)
+            {
+                _logs = new BindingList<Log>();
+                dataGridView1.DataSource = _logs;
+            }
+
+            _logs.Insert(0, new Log { EcSite = ecSite, LogDate = logDate, Status = status, Message = message });
+        }
+
+        private void dataGridView1_CellFormatting(object sender, DataGridViewCellFormattingEventArgs e)
+        {
+            if (e.RowIndex == -1 || e.ColumnIndex == -1) return;
+
+            if (e.ColumnIndex == 2)
+            {
+                if (e.Value.ToString() == "Warning")
+                {
+                    DataGridViewCellStyle cellStyle = new DataGridViewCellStyle() { BackColor = System.Drawing.Color.Yellow, ForeColor = System.Drawing.Color.Black };
+                    dataGridView1.Rows[e.RowIndex].DefaultCellStyle = cellStyle;
+                }
+                else if (e.Value.ToString() == "Error")
+                {
+                    DataGridViewCellStyle cellStyle = new DataGridViewCellStyle() { BackColor = System.Drawing.Color.Red, ForeColor = System.Drawing.Color.White };
+                    dataGridView1.Rows[e.RowIndex].DefaultCellStyle = cellStyle;
+                }
+            }
+        }
+    }
+}
---
+++ RB10.Bot.Library/Configuration.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RB10.Bot.Library
+{
+    public class Configuration
+    {
+        public class CommonConfig
+        {
+            public string ToMailAddress { get; set; }
+        }
+
+        public class EcSiteConfig
+        {
+            public class AmazonConfig
+            {
+                public string ItemUrl { get; set; }
+                public string ShopName { get; set; }
+                public Decimal UpperLimitPrice { get; set; }
+                public string LoginID { get; set; }
+            }
+
+            public class RakutenConfig
+            {
+                public string LoginID { get; set; }
+            }
+
+            public AmazonConfig Amazon { get; set; }
+            public RakutenConfig Rakuten { get; set; }
+        }
+
+        public CommonConfig Common { get; set; }
+        public EcSiteConfig EcSite { get; set; }
+
+        public Configuration()
+        {
+
+        }
+
+        public Configuration(string fileName)
+        {
+            var config = Deserialize<Configuration>(fileName);
+            Common = config.Common;
+            EcSite = config.EcSite;
+        }
+
+        private T Deserialize<T>(string fileName)
+        {
+            System.Xml.Serialization.XmlSerializer serializer = new System.Xml.Serialization.XmlSerializer(typeof(T));
+            using (System.IO.StreamReader sr = new System.IO.StreamReader(fileName, new System.Text.UTF8Encoding(false)))
+            {
+                return (T)serializer.Deserialize(sr);
+            }
+        }
+    }
+}
